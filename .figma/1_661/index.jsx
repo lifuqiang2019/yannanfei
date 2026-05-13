fb14f30fff0bb8f1d818c1aa7fb68020baf08a2b
李小俊5555 <>
2026-05-13 16:09:55 +0800

    feat:观测图谱调整

---
+++ .figma/1_661/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <img src="../image/mp3qojgv-jshf2qg.png" className={styles.frame} />
+  );
+}
+
+export default Component;
---
+++ .figma/46_553/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.group15521}>
+      <p className={styles.text}>2026 AutoNavi - GS(2019)756号</p>
+    </div>
+  );
+}
+
+export default Component;
